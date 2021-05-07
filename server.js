91306fbdd6d542c9187058a9e01e84d72843d43f
Joha2001 <57204571+Joha2001@users.noreply.github.com>
2021-05-07 10:01:49 -0400

    Additional changes for production test

--- server.js
+++ server.js
@@ -64,6 +64,11 @@ router.post('/send', (req, res, next) => {
 const app = express()
 app.use(cors())
 app.use(express.json())
-app.use(express.static(__dirname + '/'));
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.join('build', 'index.html'));
+  });
+}
 app.use('/', router)
 app.listen(process.env.PORT || 3002)
